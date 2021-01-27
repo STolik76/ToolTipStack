7ade08a49ba5da6b4dca33ca28c3b53d6369525f
STolik76 <stolik76@mail.ru>
2021-01-27 13:55:39 +0700

    start

---
+++ WpfApp1/ToolTip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Threading;
+namespace WpfApp1{
+    public delegate void DClosedToolTip(int aNum);
+    public class CToolTip : IDisposable {
+        public string Message { get; set; }
+        public string Title { get; set; }
+        public int Num { get; set; }
+        private ToolTipStack Owner;
+        private bool disposed = false;
+        private Timer timer;
+        public CToolTip(string aMessage, string aTitle, int aDelay, int aNum, ToolTipStack aOwner) {
+            Message = aMessage;
+            Title = aTitle;
+            Num = aNum;           
+            Owner = aOwner;
+            timer = new Timer(aDelay);
+            timer.Elapsed += TimerElapsed;   
+            timer.Start();
+        }
+        ~CToolTip() {
+            Dispose(false);
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e) {
+            timer.Stop();
+            Owner.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new DClosedToolTip(Owner.Remove), Num);
+        }
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool aDisposing) {
+            if (!disposed) {
+                if (aDisposing) 
+                    timer.Stop();
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ WpfApp1/ToolTipStack.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Glass.xaml
+    /// </summary>
+    public partial class ToolTipStack: Window {
+        private int count;
+        public ObservableCollection<CToolTip> Items { get; set; }
+        public ToolTipStack() {
+            InitializeComponent();
+            Items = new ObservableCollection<CToolTip>();
+            lbToolTip.ItemsSource = Items;
+        }
+        public void Push(string aMessage, string aTitle = "", int aDelay = 3000) {
+            CToolTip t = new CToolTip(aMessage, aTitle, aDelay, count, this);
+            count++;
+            Items.Add(t);
+        }
+        public void Remove(int aNum) {
+            Debug.WriteLine(aNum.ToString());
+            Items.Remove(Items.Single(x => x.Num == aNum));
+         }
+        private void Close_Button_Click(object sender, RoutedEventArgs e) {
+            Control c = (Control)sender;
+            int n = Convert.ToInt32(c.Tag);
+            CToolTip tp = Items.Single(x => x.Num == n);
+            tp.Dispose();
+            Remove(n);
+            Debug.WriteLine(n.ToString());
+        }
+
+    }
+}
